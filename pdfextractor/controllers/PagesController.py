f1343124e7639c85d202cd25f7ad904b31121e27
Jonathan CASSAING <jonathan.cassaing@free.fr>
2021-10-20 20:40:19 +0200

    Adding upload function

--- pdfextractor/controllers/PagesController.py
+++ pdfextractor/controllers/PagesController.py
@@ -2,11 +2,34 @@
 # Authors: Jonathan CASSAING
 # Tool for parsing and extracting PDF file content
 
+import os
+from flask import flash, redirect, url_for
+from flask import current_app as app
+from werkzeug.utils import secure_filename
+
 class PagesController:
+    def __allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
     def index(request):
         if request.method == 'POST':
-            print(request.form)
-            return b'data received'
+            #print(request.form)
+            #return b'data received'
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and PagesController.__allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('router.index', filename=filename))
+            return b"ERROR"
         else:
             return b"""
             <!doctype html>
