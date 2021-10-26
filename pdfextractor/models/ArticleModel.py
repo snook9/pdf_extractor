3e5c008e1b1942ecb007084327e56821d18e77f7
Jonathan CASSAING <jonathan.cassaing@student-cs.fr>
2021-10-26 10:22:00 +0200

    Extracting PDF meta data

--- pdfextractor/models/ArticleModel.py
+++ pdfextractor/models/ArticleModel.py
@@ -2,7 +2,10 @@
 # Authors: Jonathan CASSAING
 # Tool for parsing and extracting PDF file content
 
+# pdftotext is used to extract PDF content (text body)
 import pdftotext
+# PyPDF2 is used to extract PDF meta data
+from PyPDF2 import PdfFileReader
 import json
 from datetime import datetime
 from pathlib import Path
@@ -23,19 +26,55 @@ class ArticleModel(Base):
     id=Column('id', Integer, primary_key=True)
     # Datetime column in the database
     datetime=Column('datetime', String(255))
+    # Author PDF meta data
+    author=Column('author', String(255))
+    # Creator PDF meta data
+    creator=Column('creator', String(255))
+    # Producer PDF meta data
+    producer=Column('producer', String(255))
+    # Subjet PDF meta data
+    subject=Column('subject', String(255))
+    # Title PDF meta data
+    title=Column('title', String(255))
+    # Pages count PDF meta data
+    number_of_pages=Column('number_of_pages', Integer)
+    # Raw informations PDF meta data
+    raw_info=Column('raw_info', String())
     # Content column in the database
     content=Column('content', String)
     # Internal ID is used to store the real ID (in database) after the session close
     _internal_id=None
 
-    def __init__(self: object, datetime: str=None, content: str=None):
+    def __init__(self: object, datetime: str=None, 
+                author: str=None,
+                creator: str=None,
+                producer: str=None,
+                subject: str=None,
+                title: str=None,
+                number_of_pages: int=None,
+                raw_info: str=None,
+                content: str=None):
         """Initialize the object
 
         Args:
             datetime (str, optional): to force datetime. Defaults to None.
+            author (str, optional): to force author. Defaults to None.
+            creator (str, optional): to force creator. Defaults to None.
+            producer (str, optional): to force producer. Defaults to None.
+            subject (str, optional): to force subject. Defaults to None.
+            title (str, optional): to force title. Defaults to None.
+            number_of_pages (int, optional): to force number_of_pages. Defaults to None.
+            raw_info (str, optional): to force raw_info. Defaults to None.
             content (str, optional): to force content. Defaults to None.
         """
         self.datetime = str(datetime)
+        self.author = str(author)
+        self.creator = str(creator)
+        self.producer = str(producer)
+        self.subject = str(subject)
+        self.title = str(title)
+        self.number_of_pages = number_of_pages
+        self.raw_info = str(raw_info)
         self.content = str(content)
 
         # Configure the folder where text files will be saved
@@ -44,15 +83,37 @@ class ArticleModel(Base):
             # If the folder doesn't exist, we create it
             self._output_folder.mkdir()
 
-    def _persist(self, datetime: str, content: str):
+    def _persist(self, datetime: str, 
+                author: str,
+                creator: str,
+                producer: str,
+                subject: str,
+                title: str,
+                number_of_pages: int,
+                raw_info: str,
+                content: str):
         """Private method to persist the object in the database
 
         Args:
             datetime (str): datetime field
+            author (str): author field
+            creator (str): creator field
+            producer (str): producer field
+            subject (str): subject field
+            title (str): title field
+            number_of_pages (int): number_of_pages field
+            raw_info (str): raw_info field
             content (str): content field
         """
         session = session_factory()
         self.datetime = str(datetime)
+        self.author = str(author)
+        self.creator = str(creator)
+        self.producer = str(producer)
+        self.subject = str(subject)
+        self.title = str(title)
+        self.number_of_pages = number_of_pages
+        self.raw_info = str(raw_info)
         self.content = str(content)
         session.add(self)
         session.commit()
@@ -61,7 +122,7 @@ class ArticleModel(Base):
         session.close()
 
     def persist(self, filename: str):
-        """Public method to extract then persist a PDF file content in the database
+        """Public method to extract then persist a PDF file content/meta info in the database
 
         Args:
             filename (str): filename of the target file
@@ -75,10 +136,32 @@ class ArticleModel(Base):
 
         if "pdf" == filename.rsplit('.', 1)[1].lower():
             with open(filename, "rb") as f:
+                # Extracting the text (content)
                 data = pdftotext.PDF(f)
+
+                # Extracting meta data
+                pdf = PdfFileReader(f)
+                info = pdf.getDocumentInfo()
+                number_of_pages = pdf.getNumPages()
+                author = info.author
+                creator = info.creator
+                producer = info.producer
+                subject = info.subject
+                title = info.title
+
                 with open(output_filepath, 'w') as f:
+                    # Saving content to a text file
                     f.write('\n'.join(data))
-                    self._persist(today, ''.join(data))
+                    # Saving content AND meta data to the database
+                    self._persist(today,
+                                author,
+                                creator,
+                                producer,
+                                subject,
+                                title,
+                                number_of_pages,
+                                info,
+                                ''.join(data))
                     return self._internal_id
         return None
 
@@ -95,6 +178,13 @@ class ArticleEncoder(json.JSONEncoder):
             return {
                 "id": id,
                 "datetime": o.datetime,
+                "author": o.author,
+                "creator": o.creator,
+                "producer": o.producer,
+                "subject": o.subject,
+                "title": o.title,
+                "number_of_pages": o.number_of_pages,
+                "raw_info": o.raw_info,
                 "content" : o.content
                 } 
         else:
